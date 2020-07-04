1bebf126baa8bcf665d5bc6ab2b5b79fb596fa71
Michael Romay <amerror@gmail.com>
2020-07-04 14:37:27 -0700

    brought back in old portfolio site stuff

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,6 +1,32 @@
 import React from 'react';
+import styles from './App.css';
 
-export default function App() {
-  return <h1>Hello World</h1>;
+function App() {
+  return (
+    <div className={styles.App}>
+      <h1 className={styles.rainbowText}>mikey's cool dev site</h1>
+      <img src="https://vernsworld.neocities.org/under_construction_graphic.gif" />
+      <br></br>
+      <a
+        href="http://github.com/mikeymasonic"
+        rel="noopener noreferrer nofollow"
+        target="_blank"
+      >
+        github
+      </a>
+      <br></br>
+      <a
+        href="https://www.linkedin.com/in/michaelromay/"
+        rel="noopener noreferrer nofollow"
+        target="_blank"
+      >
+        linked-in
+      </a>
+      <br></br>
+      <a href="mailto:hi@michaelromay.dev">hi@michaelromay.dev</a>
+      <br></br>
+    </div>
+  );
 }
-  
+
+export default App;

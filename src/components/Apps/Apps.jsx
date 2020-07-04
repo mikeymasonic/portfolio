cc8744919f809f37dbaca6db54b9425edcc23878
Michael Romay <amerror@gmail.com>
2020-07-04 15:39:16 -0700

    started building out header created function component for apps jsx

---
+++ src/components/Apps/Apps.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Apps = () => {
+  return (
+    <>
+      <section>
+        <h1>Apps</h1>
+      </section>
+    </>
+  );
+};
+
+export default Apps;
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,23 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import styles from './Header.css';
+import logo from '../../assets/images/logo.png';
 
 const Header = () => {
   return (
     <>
-      <section>
-        <h1>Header</h1>
+      <section className={styles.Header}>
+        <section className={styles.Logo}>
+          <img src={logo} alt="michael romay logo" />
+        </section>
+        <nav className={styles.navbar}>
+          <Link className={styles.navLink} to="/apps">
+            _APPS
+          </Link>
+          <Link className={styles.navLink} to="/hardware">
+            _HARDWARE
+          </Link>
+        </nav>
       </section>
     </>
   );

526b60f258f53a01702293c48dcb0f7f11c0dd5c
Michael Romay <amerror@gmail.com>
2020-07-04 17:29:40 -0700

    added instagram icon to header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import githubIcon from '../../assets/images/Github-Icon.png';
+import linkedInIcon from '../../assets/images/LinkedIn-Icon.png';
+import instagramIcon from '../../assets/images/instagram.jpg';
 import styles from './Header.css';
 import logo from '../../assets/images/logo.png';
 
@@ -10,6 +13,40 @@ const Header = () => {
         <Link className={styles.Logo} to="/">
           <img src={logo} />
         </Link>
+
+        <section className={styles.links}>
+          <a
+            href="https://github.com/mikeymasonic"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img src={githubIcon} alt="github-icon" className={styles.icons} />
+          </a>
+          <a
+            href="https://www.linkedin.com/in/michaelromay/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img
+              src={linkedInIcon}
+              alt="linkedin-icon"
+              className={styles.icons}
+            ></img>
+          </a>
+
+          <a
+            href="https://www.instagram.com/mikeymasonic/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <img
+              src={instagramIcon}
+              alt="instagram-icon"
+              className={styles.icons}
+            ></img>
+          </a>
+        </section>
+
         <nav className={styles.navbar}>
           <Link className={styles.navLink} to="/">
             _BIO

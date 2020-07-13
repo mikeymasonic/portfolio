ff658c091459a3dca9afdaa7c51f87d8b08756c2
Michael Romay <amerror@gmail.com>
2020-07-12 18:23:27 -0700

    installation list working with arrays of images now

--- src/components/Installations/Installations.jsx
+++ src/components/Installations/Installations.jsx
@@ -1,11 +1,17 @@
 import React from 'react';
+import { installationsData } from '../../data/data';
+import InstallationsList from './InstallationsList';
 import styles from './Installations.css';
 
 const Installations = () => {
+  const installationNodes = installationsData.map((installation) => {
+    return <InstallationsList key={installation.name} {...installation} />;
+  });
   return (
     <>
       <section className={styles.Installations}>
         <h1>_INSTALLATIONS</h1>
+        {installationNodes}
       </section>
     </>
   );
